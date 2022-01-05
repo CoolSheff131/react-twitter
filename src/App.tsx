8d68dcc8aa2bfab47264bfaa14b405ddac12538f
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-01-05 14:46:25 +0800

    Созданы стили

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from '@material-ui/core/Button'
+import SignIn from './pages/SignIn';
 
 
 function App() {
   return (
     <div className="App">
-      <Button >AS</Button >
+      <SignIn></SignIn>
     </div>
   );
 }
