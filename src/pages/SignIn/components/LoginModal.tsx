d434c2821bf05c86a5cf1afa1609ca34af380b96
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-01-12 17:22:15 +0800

    логин на бэк и нотифицации

--- src/pages/SignIn/components/LoginModal.tsx
+++ src/pages/SignIn/components/LoginModal.tsx
@@ -5,6 +5,9 @@ import ModalBlock from '../../../components/ModalBlock'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from "yup"
 import { Controller, useForm } from 'react-hook-form'
+import { AuthApi } from '../../../services/api/authApi'
+import { Notification } from '../../../components/Notification'
+import { Color } from '@material-ui/lab'
 
 const LoginFormSchema = yup.object().shape({
     email: yup.string().email('Неверная почта').required('Введите почту'),
@@ -17,7 +20,7 @@ interface LoginModalProps {
     onClose: () => void;
 }
 
-interface LoginFormProps {
+export interface LoginFormProps {
     email: string;
     password: string;
 }
@@ -27,74 +30,88 @@ export const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }): React.
     const { control, formState: { errors }, handleSubmit } = useForm<LoginFormProps>({
         resolver: yupResolver(LoginFormSchema)
     })
-    const onSubmit = (data: LoginFormProps) => console.log(data);
+    const onSubmit = async (openNotification: (text: string, type: Color) => void, data: LoginFormProps) => {
+
+        try {
+            const userData = await AuthApi.signIn(data)
+            console.log(userData);
+            openNotification('Авторизация прошла успешна', 'success')
+        } catch (error) {
+            openNotification('Неверный логин или пароль', 'error')
+        }
+    }
+
     console.log(errors);
 
 
     return (
-        <ModalBlock visible={open} onClose={onClose} title="Войти в аккаунт" classes={classes}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <FormControl className={classes.loginFormControl} component="fieldset" fullWidth>
-                    <FormGroup aria-label="position" row>
-                        <Controller
-                            name="email"
-                            control={control}
-                            render={({
-                                field: { onChange, onBlur, value, name, ref },
-                                fieldState: { invalid, isTouched, isDirty, error },
-                                formState,
-                            }) => (
-                                <TextField autoFocus
-                                    className={classes.loginSideField}
-                                    margin='dense'
+        <Notification>
+            {openNotification => (<ModalBlock visible={open} onClose={onClose} title="Войти в аккаунт" classes={classes}>
+                <form onSubmit={handleSubmit(onSubmit.bind(null, openNotification))}>
+                    <FormControl className={classes.loginFormControl} component="fieldset" fullWidth>
+                        <FormGroup aria-label="position" row>
+                            <Controller
+                                name="email"
+                                control={control}
+                                render={({
+                                    field: { onChange, onBlur, value, name, ref },
+                                    fieldState: { invalid, isTouched, isDirty, error },
+                                    formState,
+                                }) => (
+                                    <TextField autoFocus
+                                        className={classes.loginSideField}
+                                        margin='dense'
 
-                                    label="Email Address"
-                                    InputLabelProps={{ shrink: true }}
-                                    variant="filled"
-                                    type="email"
-                                    fullWidth
-                                    error={!!errors.email}
-                                    helperText={error?.message}
-                                    onBlur={onBlur} // notify when input is touched
-                                    onChange={onChange} // send value to hook form
-                                    inputRef={ref}
-                                />
-                            )}
-                        />
-                        <Controller
-                            name="password"
-                            control={control}
-                            render={({
-                                field: { onChange, onBlur, value, name, ref },
-                                fieldState: { invalid, isTouched, isDirty, error },
-                                formState,
-                            }) => (
-                                <TextField autoFocus
+                                        label="Email Address"
+                                        InputLabelProps={{ shrink: true }}
+                                        variant="filled"
+                                        type="email"
+                                        fullWidth
+                                        error={!!errors.email}
+                                        helperText={error?.message}
+                                        onBlur={onBlur} // notify when input is touched
+                                        onChange={onChange} // send value to hook form
+                                        inputRef={ref}
+                                    />
+                                )}
+                            />
+                            <Controller
+                                name="password"
+                                control={control}
+                                render={({
+                                    field: { onChange, onBlur, value, name, ref },
+                                    fieldState: { invalid, isTouched, isDirty, error },
+                                    formState,
+                                }) => (
+                                    <TextField autoFocus
 
-                                    className={classes.loginSideField}
-                                    margin='dense'
+                                        className={classes.loginSideField}
+                                        margin='dense'
 
-                                    label="Пароль"
-                                    InputLabelProps={{ shrink: true }}
-                                    variant="filled"
-                                    type="password"
-                                    error={!!error}
-                                    helperText={error?.message}
-                                    fullWidth
-                                    onBlur={onBlur} // notify when input is touched
-                                    onChange={onChange} // send value to hook form
-                                    inputRef={ref}
-                                />
-                            )}
-                        />
+                                        label="Пароль"
+                                        InputLabelProps={{ shrink: true }}
+                                        variant="filled"
+                                        type="password"
+                                        error={!!error}
+                                        helperText={error?.message}
+                                        fullWidth
+                                        onBlur={onBlur} // notify when input is touched
+                                        onChange={onChange} // send value to hook form
+                                        inputRef={ref}
+                                    />
+                                )}
+                            />
 
 
-                        <Button type="submit" variant='contained' color="primary" fullWidth>
-                            Войти
-                        </Button>
-                    </FormGroup>
-                </FormControl>
-            </form>
-        </ModalBlock>
+                            <Button type="submit" variant='contained' color="primary" fullWidth>
+                                Войти
+                            </Button>
+                        </FormGroup>
+                    </FormControl>
+                </form>
+            </ModalBlock>)}
+        </Notification>
+
+
     )
 }
